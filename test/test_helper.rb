f7e60fb4ba0ecee88c1e5f8b140abef3b9fa3c87
WMW <wnameless@gmail.com>
2013-09-12 22:39:21 +0800

    Remove whitespaces

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,3 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require "transformer"
- 
--- test/transformer_test.rb
+++ test/transformer_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class TransformerTest  < Test::Unit::TestCase
-   TEST_CSV = File.join File.dirname(__FILE__), 'test.csv'
-   
-   def test_csv2hashes
-     assert_equal [{"A"=>"2", "B"=>"4"}], Transformer.csv2hashes(TEST_CSV , :separator => '|')
-   end
-   
-   def test_hashes2csv
-     assert_equal "A|B\n2|4\n", Transformer.hashes2csv([{A: 2,B:4}], :separator => '|')
-   end
+class TransformerTest < Test::Unit::TestCase
+  TEST_CSV = File.join File.dirname(__FILE__), 'test.csv'
+  
+  def test_csv2hashes
+    assert_equal [{"A"=>"2", "B"=>"4"}], Transformer.csv2hashes(TEST_CSV, :separator => '|')
+  end
+  
+  def test_hashes2csv
+    assert_equal "A|B\n2|4\n", Transformer.hashes2csv([{A: 2,B:4}], :separator => '|')
+  end
 end
