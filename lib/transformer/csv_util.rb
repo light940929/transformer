0e0d429151f0a0bb9224f283069f98a88696fe31
WMW <wnameless@gmail.com>
2013-09-12 21:56:36 +0800

    Finish revise

--- lib/transformer/csv_util.rb
+++ lib/transformer/csv_util.rb
@@ -1,13 +1,12 @@
-require "csv"
 module Transformer
   module CsvUtil
     def csv2hashes path, opts = {}
       hash_ary = []
       separator = opts[:separator] || ','
       f = File.open path, 'r'
-      header = f.each.first.split separator
+      header = f.each.first.strip.split separator
       f.each do |line|
-        hash_ary << Hash[ header.zip line.split(separator) ]
+        hash_ary << Hash[ header.zip line.strip.split(separator) ]
       end
       f.close
       hash_ary
--- test/transformer_test.rb
+++ test/transformer_test.rb
@@ -1,20 +1,13 @@
-require "test_helper"
-require "transformer/csv2hash.rb"
-require "transformer/hash2csv.rb"
-require "transformer/csv2hash.rb"
-
+require 'test_helper'
 
 class TransformerTest  < Test::Unit::TestCase
+   TEST_CSV = File.join File.dirname(__FILE__), 'test.csv'
    
-   def setup
-      csv = Transformer::Csv.new
-      hash =  Transformer::Hash.new
+   def test_csv2hashes
+     assert_equal [{"A"=>"2", "B"=>"4"}], Transformer.csv2hashes(TEST_CSV , :separator => '|')
    end
    
-   
-   #def test_csv assert csv.kind_of ? Transformer::Csv.csv2hash ; end
-   
-   #def test_hash assert hash.kind_of ? Transformer::Hash.hash2csv ; end
-     
-
+   def test_hashes2csv
+     assert_equal "A|B\n2|4\n", Transformer.hashes2csv([{A: 2,B:4}], :separator => '|')
+   end
 end
